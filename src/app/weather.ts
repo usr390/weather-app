4c96c53afdc76d03e5c62afb04c3e1b878beb0c2
vendingcompute <edutre1999@gmail.com>
2022-02-11 20:08:50 -0600

    Modified weather interface: defined initial properties

--- src/app/weather.ts
+++ src/app/weather.ts
@@ -1,2 +1,27 @@
 export interface Weather {
+    
+    weather: WeatherInfo[],
+    
+    main: {
+        temp: number;
+        pressure: number;
+        humidity: number;
+    };
+    
+    wind: {
+        speed: number;
+    };
+    
+    sys: {
+        country: string
+    };
+    
+    name: string;
 }
+
+interface WeatherInfo {
+    main: string;
+    icon: string;
+}
+
+
