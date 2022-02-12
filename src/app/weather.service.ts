cdc082c6db225fd7e5c5e3378dcb1754f64d342f
vendingcompute <edutre1999@gmail.com>
2022-02-11 22:20:46 -0600

    Modified weather service: introduced comments

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -14,8 +14,11 @@ export class WeatherService {
 
   getWeather(city: string): Observable<Weather> {
 
+    // we use the constructor of the HttpParams object and call its set method for each query parameter we want to add to the URL
     const options = new HttpParams().set('units', 'metric').set('q', 'city').set('appId', environment.apiKey);
 
+    // we use the get method of the HttpClient service. it accepts two parameters: the url endpoint to our api, and an options
+    // object which provides additional configuration to the request
     return this.http.get<Weather>(environment.apiUrl + 'weather', {params: options})
 
   }
