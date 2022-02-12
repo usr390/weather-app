34a6189b30c7fb33812669f8bccc6af21d1ef96b
vendingcompute <edutre1999@gmail.com>
2022-02-11 21:29:13 -0600

    Modified weather service: injected http service to weather service constructor

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,5 @@ import { Injectable } from '@angular/core';
 })
 export class WeatherService {
 
-  constructor() { }
+  constructor(private http: HttpClientModule) { }
 }
