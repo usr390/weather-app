2b4cf29742fcf14c3cf542f56e863bd267b09602
vendingcompute <edutre1999@gmail.com>
2022-02-11 23:21:18 -0600

    Modified weather component: imported weather interface and weather service

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Weather } from '../weather';
+import { WeatherService } from '../weather.service';
 
 @Component({
   selector: 'app-weather',
